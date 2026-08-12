992ced45806b097eb17e7e963583d59953286905
Ayush Verma <ayushverma19341@gmail.com>
2026-08-13 01:39:27 +0530

    Update 5-LARGEST.C

--- LAB PROGRAMS_/LAB 3/5-LARGEST.C
+++ LAB PROGRAMS_/LAB 3/5-LARGEST.C
@@ -3,28 +3,15 @@ Write a generic function to find the largest element in an array of any data typ
 */
 #include <stdio.h>
 
-int compareInt(void *a, void *b)
-{
-    int x = *(int *)a;
-    int y = *(int *)b;
-
-    if (x > y)
-        return 1;
-    else if (x < y)
-        return -1;
-    else
-        return 0;
-}
-
 int Max(void *array, int n, int size)
 {
-    char *arr = array;
+    int *arr = array;
     int max = 0;
     int i;
 
     for (i = 1; i < n; i++)
     {
-        if (compareInt(arr + i * size, arr + max * size) > 0)
+        if (arr[i] > arr[max])
             max = i;
     }
 
@@ -33,9 +20,18 @@ int Max(void *array, int n, int size)
 
 int main()
 {
-    int arr[] = {10, 50, 20, 80, 30};
+    int arr[100];
+    int n, i, index;
 
-    int index = Max(arr, 5, sizeof(int));
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    index = Max(arr, n, sizeof(int));
 
     printf("Largest element = %d", arr[index]);
 
